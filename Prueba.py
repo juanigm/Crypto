bf60c1fb89db1d2077595a9d2da25e027cc2d2d5
juani <juanigm99@gmail.com>
2022-04-09 23:40:18 -0300

    APP FINISHED

--- Prueba.py
+++ Prueba.py
@@ -2,7 +2,7 @@ import pywhatkit
 # Usamos Un try-except
 try:
   # Enviamos el mensaje
-  pywhatkit.sendwhatmsg("+542324476498",  
+  pywhatkit.sendwhatmsg("phonenumber",  
                         "Mensaje De Prueba",
                         10,00)
   print("Mensaje Enviado")
